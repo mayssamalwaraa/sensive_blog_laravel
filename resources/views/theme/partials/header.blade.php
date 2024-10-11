bd69e4df522cf53373f9536498106bf666daa04c
Mayssam Alwaraa <mayssamalwaraa295@gmail.com>
2024-10-11 17:52:22 +0100

    Authentication section

--- resources/views/theme/partials/header.blade.php
+++ resources/views/theme/partials/header.blade.php
@@ -30,14 +30,25 @@
             <!-- End - Add new blog -->
 
             <ul class="nav navbar-nav navbar-right navbar-social">
-              <a href="#" class="btn btn-sm btn-warning">Register / Login</a>
-              <!-- <li class="nav-item submenu dropdown">
+              @if (!Auth::check())
+              <a href="{{ route('register') }}" class="btn btn-sm btn-warning">Register / Login</a>
+              @else
+              <li class="nav-item submenu dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Welcome User</a>
+                  aria-expanded="false">{{ Auth::user()->name }}</a>
                 <ul class="dropdown-menu">
                   <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
+                  <li class="nav-item">
+                  <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <a class="nav-link" href="javascript:$('form').submit();">Log out</a>
+                  </form>
+                  </li>
                 </ul>
-              </li> -->
+              </li> 
+              @endif
+              
+               
             </ul>
           </div> 
         </div>
